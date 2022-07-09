ba8ed2f17dee121699ad638e754aa7493525b990
Darrian Bagley <dbagle14@gmail.com>
2022-07-09 13:37:09 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_contains_clickbait
+
+  def title_contains_clickbait
+    if !(/Won't Believe/.match?(title) || /Top \d/.match?(title) || /Guess/.match?(title))
+      errors.add(:title, "must contain \"won\"t believe\", \"top 10\", or \"guess\"")
+    end
+  end
 end
